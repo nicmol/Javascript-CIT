7c2e0d2a4bc1e13da3b7d9c506ce1fb75182b846
nicmol <nicmol27@yahoo.com>
2019-10-22 17:22:40 -0700

    added Header component

---
+++ src/Header.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+// import { connect } from 'react-redux';
+import './App.css';
+
+class Header extends Component{
+ render() {
+   return(
+      <div class="title">
+      <div class="container text-center">
+        <h1>Lane Community College CIT Lab</h1>
+      </div>
+    </div>
+    );
+ }
+}
+export default Header;
